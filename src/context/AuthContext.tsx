79be2714efe34b819a7d8b64c1cf27475564340d
Tahsin <alahitahsin@gmail.com>
2024-11-08 07:37:01 +0600

    update: added unsubscibe to listen

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,18 +1,25 @@
 import {
   createUserWithEmailAndPassword,
+  onAuthStateChanged,
   signInWithEmailAndPassword,
+  User,
 } from "firebase/auth";
-import React, { createContext, useContext } from "react";
+import React, { createContext, useContext, useEffect, useState } from "react";
 import { firebaseAuth } from "../firebase/firebase.auth";
 
 interface AuthContext {
   register: (email: string, password: string) => Promise<void>;
   loginWithEmail: (email: string, password: string) => Promise<void>;
+  user: User | null;
+  isAuthLoading: boolean;
 }
 
 const AuthContext = createContext<AuthContext | undefined>(undefined);
 
 function AuthProvider({ children }: { children: React.ReactNode }) {
+  const [user, setUser] = useState<AuthContext["user"]>(null);
+  const [isAuthLoading, setIsAuthLoading] = useState(true);
+
   async function register(email: string, password: string) {
     try {
       await createUserWithEmailAndPassword(firebaseAuth, email, password);
@@ -23,17 +30,27 @@ function AuthProvider({ children }: { children: React.ReactNode }) {
 
   async function loginWithEmail(email: string, password: string) {
     try {
-      const res = await signInWithEmailAndPassword(
-        firebaseAuth,
-        email,
-        password
-      );
+      await signInWithEmailAndPassword(firebaseAuth, email, password);
     } catch (err) {
       console.log(err);
     }
   }
 
-  const value = { register, loginWithEmail };
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(
+      firebaseAuth,
+      (user: AuthContext["user"]) => {
+        setUser(user);
+        setIsAuthLoading(false);
+      }
+    );
+
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
+  const value = { register, loginWithEmail, user, isAuthLoading };
 
   return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 }
